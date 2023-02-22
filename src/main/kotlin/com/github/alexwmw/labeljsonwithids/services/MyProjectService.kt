de77a2f761b942f2fd441289871a2ba2e8627449
GitHub Action <action@github.com>
2023-02-22 11:09:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/alexwmw/labeljsonwithids/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alexwmw/labeljsonwithids/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.alexwmw.labeljsonwithids.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alexwmw.labeljsonwithids.MyBundle
 
 class MyProjectService(project: Project) {
 
